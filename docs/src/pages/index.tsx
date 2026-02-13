11f33ec99c48cf0b896c7a837e9dbe5e2a5da33d
masonlavinder <mlavinder@mpr.com>
2026-02-13 16:21:32 -0500

    update /

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,45 +1,32 @@
 import type { ReactNode } from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
 
 import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Documentation - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+      <main className={styles.splash}>
+        <Heading as="h1" className={styles.title}>
+          {siteConfig.title}
+        </Heading>
+        <p className={styles.tagline}>{siteConfig.tagline}</p>
+        <a
+          className={styles.cta}
+          href="https://3rd-loop.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Get Started
+        </a>
+        <br />
+        <p>
+          note this button should just immediately route to 3rd-loop.com
+          eventually
+        </p>
       </main>
     </Layout>
   );
