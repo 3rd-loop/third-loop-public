50e4fbf223d6261606130e5f12b088e61a7da478
masonlavinder <mlavinder@mpr.com>
2026-02-25 16:44:02 -0500

    udpate config for custom url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://thirdloop.dev",
+  url: "https://docs.3rd-loop.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
