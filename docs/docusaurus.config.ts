fd8f3b6202dfa56bf039dc89d2238f24ae6796e0
masonlavinder <mlavinder@mpr.com>
2026-03-24 16:42:11 -0400

    bug fix

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -229,7 +229,7 @@ const config: Config = {
           items: [
             {
               label: "Documentation",
-              to: "/intro",
+              to: "/",
             },
           ],
         },
